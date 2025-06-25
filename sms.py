d02f6655117ac71be7f929434b8931b4ea16493c
Sumit Kumar <154729416+Sumitait@users.noreply.github.com>
2025-06-25 10:28:38 +0530

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,63 @@
+
+import csv, sys 
+import time
+from twilio.rest import Client
+
+MESSAGE_FILE = 'message.txt'     # File containing text message
+CSV_FILE = 'participants.csv'    # File containing participant numbers
+SMS_LENGTH = 160                 # Max length of one SMS message
+MSG_COST = 0.04                  # Cost per message
+TIMEOUT_SECONDS = 2              # Sleep time after each text
+
+# Twilio: Find these values at https://twilio.com/user/account
+account_sid = "AC5396757039ec0ea204b3fa4ae03155fa"   # Ensure you remove the angle brackets! < >
+auth_token = "45604b22a98d76f2d234ff54ebc611e9"
+from_num = "+17542033162"                  # 'From' number in Twilio
+
+# Now put your SMS in a file called message.txt, and it will be read from there.
+with open(MESSAGE_FILE, 'r') as content_file:
+    sms = content_file.read()
+
+# Check we read a message OK
+if len(sms.strip()) == 0:
+    print("SMS message not specified- please make a {}' file containing it. \r\nExiting!".format(MESSAGE_FILE))
+    sys.exit(1)
+else:
+    print("> SMS message to send: \n\n{}".format(sms))
+
+# How many segments is this message going to use?
+segments = int(len(sms.encode('utf-8')) / SMS_LENGTH) +1
+
+# Open the people CSV and get all the numbers out of it
+with open(CSV_FILE, 'r') as csvfile:
+    peoplereader = csv.reader(csvfile)
+    numbers = set([p[0] for p in peoplereader]) # remove duplicate numbers
+
+
+# Calculate how much it's going to cost:
+messages = len(numbers)
+cost = MSG_COST * segments * messages
+
+print("> {} messages of {} segments each will be sent, at a cost of ${} ".format(messages, segments, cost))
+
+# Check you really want to send the
+#m
+confirm = input("Send these messages? [Y/n] ")
+if confirm[0].lower() == 'y':
+    # Set up Twilio client
+    client = Client(account_sid, auth_token)
+
+    # Send the messages
+    for num in numbers:
+        # Send the sms text to the number from the CSV file:
+        print("Sending to " + num)
+        message = client.messages.create(to=num, from_=from_num, body=sms)
+        time.sleep(TIMEOUT_SECONDS)
+
+print("Exiting!")
+#
+ #* sms_sender/
+#├── message.txt
+#├── participants.csv
+ # ── SmsSender.py
+#*/
